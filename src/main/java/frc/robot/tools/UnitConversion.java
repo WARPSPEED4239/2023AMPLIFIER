debd9ad9febc1f03a78b7fb6b3b45cbf43dccfe1
MiguelGarcia5605 <100482998+MiguelGarcia5605@users.noreply.github.com>
2023-01-15 16:02:14 -0600

    Update UnitConversion.java

--- src/main/java/frc/robot/tools/UnitConversion.java
+++ src/main/java/frc/robot/tools/UnitConversion.java
@@ -3,14 +3,6 @@ package frc.robot.tools;
 import edu.wpi.first.math.util.Units;
 
 public class UnitConversion {
-    public static final double PI = 3.141592;
-
-    public static final double METERS_PER_FEET = 0.3048;
-    public static final double FEET_PER_METER = 3.28084;
-    public static final double INCHES_PER_FEET = 0.0833333;
-    
-    public static final double KILOGRAMS_PER_POUND = 0.453592;
-    public static final double POUNDS_PER_KILOGRAM = 2.20462;
 
     public static final double WHEEL_DIAMETER = 6.0;
     public static final double WHEEL_DIAMETER_METERS = Units.inchesToMeters(WHEEL_DIAMETER);
@@ -19,43 +11,27 @@ public class UnitConversion {
 
     public static final int SRX_UNITS_PER_ROTATION = 4096;
 
-    public static double MetersToFeet(double meters) {
-        return meters * FEET_PER_METER;
+    public static double inchesToFeet(double inches) {
+        return inches / 12.0;
     }
 
-    public static double FeetToMeters(double feet) {
-        return feet * METERS_PER_FEET;
-    }
-
-    public static double InchesToFeet(double inches) {
-        return inches * INCHES_PER_FEET;
-    }
-
-    public static double PoundsToKilograms(double pounds) {
-        return pounds * KILOGRAMS_PER_POUND;
-    }
-
-    public static double KilogramsToPounds(double kilograms) {
-        return kilograms * POUNDS_PER_KILOGRAM;
-    }
-
-    public static double RevolutionsPerMinuteToRadiansPerSecond(double rpm) {
-        return rpm * 2.0 * PI / 60;
+    public static double revolutionsPerMinuteToRadiansPerSecond(double rpm) {
+        return rpm * 2.0 * Math.PI / 60;
     }
 
     public static double SRXUnitsToRotations(double units) {
         return units / SRX_UNITS_PER_ROTATION;
     }
 
-    public static double RotationsToSRXUnits(double rotations) {
+    public static double rotationsToSRXUnits(double rotations) {
         return rotations * SRX_UNITS_PER_ROTATION;
     }
 
-    public static double RotationsToInches(double rotations, double pulleyDiameter) {
+    public static double rotationsToInches(double rotations, double pulleyDiameter) {
         return rotations * Math.PI * pulleyDiameter;
     }
 
-    public static double PositionInInchesToRotations(double positionInInches, double pulleyDiameter) {
+    public static double positionInInchesToRotations(double positionInInches, double pulleyDiameter) {
         return positionInInches / (pulleyDiameter * Math.PI);
     }
 
@@ -63,11 +39,11 @@ public class UnitConversion {
         return (units / SRX_UNITS_PER_ROTATION) * 360;
     }
 
-    public static double PositionInDegreesToSRXUnits(double positionInDegrees) {
+    public static double positionInDegreesToSRXUnits(double positionInDegrees) {
         return (positionInDegrees / 360) * SRX_UNITS_PER_ROTATION;
     }
 
-    public static double TargetPositionInMetersToFXUnits(double targetPositionInMeters) {
+    public static double targetPositionInMetersToFXUnits(double targetPositionInMeters) {
         return (Math.PI * WHEEL_DIAMETER_METERS) / (GEARBOX_RATIO * targetPositionInMeters);
     }
 }
