38efa6546460dd7101acfd0b16932f9ea9768985
Team4239 <warpspeed@willmar.org>
2023-02-14 19:38:56 -0600

    Updated UnitConversion

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,17 +21,25 @@ public final class Constants {
         HOOK_SOLENOID_REVERSE_PORT = 9,
         ARM_LIMIT_SWITCH = 1,
 
+        SPROCKET_GEARBOX_RATIO = 30,
+
         XBOX_CONTROLLER_PORT = 0,
         JOYSTICK_PORT = 1,
 
         PIGEON_IMU = 9,
 
-        SRX_UNITS_PER_ROTATION = 4096;
+        SRX_UNITS_PER_ROTATION = 4096,
+        NEO_UNITS_PER_ROTATION = 42;
         
     public static final double
         RAMP_RATE = 0.3,
+        // neo gearbox ratio 30:1
+        // 42 counter per revolution for neo encoder
+        // for every every tick on encoder arm will move (179/157500) inches
         GEARBOX_RATIO = 7.08,
         WHEEL_DIAMETER_INCHES = 6.0,
+        SPROCKET_DIAMETER_INCHES = 1.432,
+        SPROCKET_CIRCUM = SPROCKET_DIAMETER_INCHES * Math.PI,
 
         XBOX_CONTROLLER_DEADZONE = 0.1,
 
