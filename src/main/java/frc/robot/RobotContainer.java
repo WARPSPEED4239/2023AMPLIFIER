b60dd90d471d9fcfb323f2e456ee583124fdee35
Team4239 <warpspeed@willmar.org>
2023-02-17 22:42:33 -0600

    Tweak

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.Arm.ArmSetSpeed;
@@ -63,7 +64,7 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-   return null;
+   return new InstantCommand();
   }
 
   public Slider getSlider() {
