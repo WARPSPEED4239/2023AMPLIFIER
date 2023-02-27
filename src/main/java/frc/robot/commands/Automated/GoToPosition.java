d48783dd265b1fe3b34cbef4be42e634f2f7e19f
Darren Seubert <dpseubert15@gmail.com>
2023-02-27 15:52:42 -0600

    Update GoToPosition.java

--- src/main/java/frc/robot/commands/Automated/GoToPosition.java
+++ src/main/java/frc/robot/commands/Automated/GoToPosition.java
@@ -12,7 +12,7 @@ import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Slider;
 
 public class GoToPosition extends SequentialCommandGroup {
-    public static boolean mArmFirst = true;
+    public static boolean mSliderFirst;
 
     public GoToPosition(Arm arm, Slider slider, Constants.Positions positions) {
         Arm mArm = arm;
@@ -21,40 +21,40 @@ public class GoToPosition extends SequentialCommandGroup {
         double mArmTargetInches = 150.0;
         double mSliderTargetDegrees = 0.0;
         boolean eStop = false;
-        boolean prevArmFirst = mArmFirst;
+        boolean prevSliderFirst = mSliderFirst;
 
         switch (positions) {
             case Starting: // Arm HAS to Move First Leaving this State
                 mArmTargetInches = 150.0;
                 mSliderTargetDegrees = 0.0;
-                mArmFirst = true;
+                mSliderFirst = false;
                 break;
             case Intaking: // Arm HAS to Move First Leaving this State
                 mArmTargetInches = 141.0;
                 mSliderTargetDegrees = 0.0;
-                mArmFirst = true;
+                mSliderFirst = false;
                 break;
             case LowScoring: // Slider Maybe has to Move First Leaving this State
                 mArmTargetInches = 46.0;
                 mSliderTargetDegrees = 9.0;
-                mArmFirst = false;
+                mSliderFirst = true;
                 break;
             case HighScoring: // Slider HAS to Move First Leaving this State
                 mArmTargetInches = 27.9;
                 mSliderTargetDegrees = 23.0;
-                mArmFirst = false;
+                mSliderFirst = true;
                 break;
             case Station: // Arm May Move First Leaving this State
                 mArmTargetInches = 46.0;
                 mSliderTargetDegrees = 0.0;
-                mArmFirst = true;
+                mSliderFirst = false;
                 break;
             case eStop:
                 eStop = true;
                 break;
         }
 
-        SmartDashboard.putBoolean("Next ArmFirst Value", mArmFirst);
+        SmartDashboard.putBoolean("Next SliderFirst Value", mSliderFirst);
 
         if (eStop) {
             addCommands(
@@ -63,7 +63,7 @@ public class GoToPosition extends SequentialCommandGroup {
                     new SliderSetSpeed(mSlider, 0.0)
                 ).withTimeout(0.1)
             );
-        } else if (prevArmFirst) {
+        } else if (!prevSliderFirst) {
             addCommands(
                 new ArmSetPosition(mArm, mArmTargetInches).withTimeout(1.0),
                 new ParallelCommandGroup(
