881c57781d181142f142134ba555937394bc7cd5
Darren Seubert <dpseubert15@gmail.com>
2023-02-28 18:53:58 -0600

    Update AutoBalanceV2.java

--- src/main/java/frc/robot/commands/Autonomous/AutoBalanceV2.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoBalanceV2.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.Autonomous;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Pigeon;
@@ -8,7 +9,7 @@ import frc.robot.subsystems.Shifter;
 public class AutoBalanceV2 extends CommandBase {
   private final Drivetrain mDrivetrain;
   private final Shifter mShifter;
-  private final double kP = 0.05;
+  private final double kP = -0.025;
   private double pitch;
   private double speed;
 
@@ -31,6 +32,7 @@ public class AutoBalanceV2 extends CommandBase {
     pitch = Pigeon.getRoll();
     speed = kP * pitch;
 
+    SmartDashboard.putNumber("DRIVE PERCENT OUT", speed);
     mDrivetrain.arcadeDrive(speed, 0.0);
   }
 
