09ecb3808a3994f57857fef603df7c124234daa6
Darren Seubert <dpseubert15@gmail.com>
2023-02-28 18:09:01 -0600

    Back to how it was. Thanks Dustin

--- src/main/java/frc/robot/commands/Automated/GoToPosition.java
+++ src/main/java/frc/robot/commands/Automated/GoToPosition.java
@@ -36,7 +36,7 @@ public class GoToPosition extends SequentialCommandGroup {
                 mSliderTargetInches = 23.0;
                 break;
             case Station: // Arm May Move First Leaving this State
-                mArmTargetDegrees = 45.5;
+                mArmTargetDegrees = 45.0;
                 mSliderTargetInches = 0.0;
                 break;
             case eStop:
