8a2af4049746699c6ec7f9deb89ef5cdb0bfd081
Darren Seubert <dpseubert15@gmail.com>
2023-03-09 13:33:51 -0600

    Update AutonomousCommand.java

--- src/main/java/frc/robot/commands/Autonomous/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/Autonomous/AutonomousCommand.java
@@ -90,24 +90,24 @@ public class AutonomousCommand extends SequentialCommandGroup {
         );
         break;
       case ScoreConeBackwardAutoBalance:
-      addCommands(
-        new ArmSetPosition(mArm, 153.0).withTimeout(2.0), // Arm moves up for 2 seconds
-        new GoToPosition(mArm, mSlider, Positions.HighScoring).withTimeout(2.0),  // Arm up and Slider out for 2 seconds
-        new ParallelCommandGroup(
-          new GoToPosition(mArm, mSlider, Positions.HighScoring),
-          new ClawPistonSetState(mIntakeClaw, true)
-        ).withTimeout(1.0),                                                       // Arm and Slider hold, claw release for 1 second
-        new ParallelCommandGroup(
-          new GoToPosition(mArm, mSlider, Positions.Starting),
-          new ClawPistonSetState(mIntakeClaw, false)
-        ).withTimeout(2.0),                                                       // Arm down, Slider in, claw grab for 2 seconds
-        new ParallelCommandGroup(
-          new ShifterSetState(mShifter, false),
-          new MoveWithNoSensors(mDrivetrain, -0.7, -0.15)                                // Drivetrain backwards for 1.5 seconds
-        ).withTimeout(1.5),
-        new AutoBalanceV4(mDrivetrain, mShifter, true)
-      );
-      break;
+        addCommands(
+          new ArmSetPosition(mArm, 153.0).withTimeout(2.0), // Arm moves up for 2 seconds
+          new GoToPosition(mArm, mSlider, Positions.HighScoring).withTimeout(2.0),  // Arm up and Slider out for 2 seconds
+          new ParallelCommandGroup(
+            new GoToPosition(mArm, mSlider, Positions.HighScoring),
+            new ClawPistonSetState(mIntakeClaw, true)
+          ).withTimeout(1.0),                                                       // Arm and Slider hold, claw release for 1 second
+          new ParallelCommandGroup(
+            new GoToPosition(mArm, mSlider, Positions.Starting),
+            new ClawPistonSetState(mIntakeClaw, false)
+          ).withTimeout(2.0),                                                       // Arm down, Slider in, claw grab for 2 seconds
+          new ParallelCommandGroup(
+            new ShifterSetState(mShifter, false),
+            new MoveWithNoSensors(mDrivetrain, -0.7, -0.15)                                // Drivetrain backwards for 1.5 seconds
+          ).withTimeout(1.5),
+          new AutoBalanceV4(mDrivetrain, mShifter, true)
+        );
+        break;
     }
   }
 }
